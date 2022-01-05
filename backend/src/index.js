7b53e2d92038fc69fdec64b42ba305c797718edb
Mohamed Essam <mohamedessam.moe@gmail.com>
2022-01-05 12:39:34 +0200

    add new user

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
 require("./db/db");
 const express = require("express");
+const userRoute = require("./routes/users");
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello Travel Map");
 });
+
+app.use("/api/users", userRoute);
 
 app.listen(port, () => {
   console.log("server is running on: http://localhost:" + port);
